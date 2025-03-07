ab20e2684b0dcb35f48c7aac8e916b5c969e19b7
MatteInvi <matteoinvidia@gmail.com>
2025-03-07 19:24:05 +0100

    feat: completamento parte js e aggiunta particolari html

--- js/script.js
+++ js/script.js
@@ -1,23 +1,25 @@
 // Variabili preimpostate
 const orePreimp = 10;
-// const sconto = prezzoParziale * 25/100;
 let prezzoParziale;
+let prezzoFinale;
 
 // Importazione input utente
 const name = document.getElementById("name");
 const surname = document.getElementById("surname");
 const email = document.getElementById("mail");
 const infoExtra = document.getElementById("text-area");
-
 const formPreventivo = document.getElementById("form-preventivo");
+const promoCodeUser = document.getElementById("promo-code");
+const promoCodeError = document.getElementById("promo-code-error");
 
-// prezzi orari per tipologia di lavoro
+// Prezzi orari per tipologia di lavoro
 const backendPrice = 20.50;
 const frontendPrice = 15.30;
 const projectAnalysisPrice = 33.60;
 
-//Codici sconto applicabili
-const arrayCodici = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
+// Array Codici sconto applicabili
+const arrayCodici = ["YHDNU32", "JANIC63", "PWKCN25", "JANJC63", "POCIE24"];
+
 
 // Importazione output prezzo finale
 const finalPriceElem = document.getElementById("final-price");
@@ -25,24 +27,43 @@ const finalPriceElem = document.getElementById("final-price");
 
 //Funzione al click su "calcola preventivo"
 
-formPreventivo.addEventListener("submit", function () {
-    let typeOfWork = document.getElementById("work-type");
-    const promoCode = document.getElementById("promo-code");
+formPreventivo.addEventListener("submit", function (event) {
+    event.preventDefault()
 
+    // Lettura variabile al click
+    let typeOfWork = document.getElementById("work-type");
+    
+    // Controllo tipologia di lavoro
     if (typeOfWork.value === "backend") {
-        prezzoParziale = backendPrice* orePreimp;
+        prezzoParziale = backendPrice * orePreimp;
     } else if (typeOfWork.value === "frontend") {
         prezzoParziale = frontendPrice * orePreimp;
     } else {
-        prezzoParziale =  projectAnalysisPrice * orePreimp;
+        prezzoParziale = projectAnalysisPrice * orePreimp;
     }
-    console.log(prezzoParziale);
 
-    arrayCodici.forEach(function(code){
-        console.log(promoCode);
-        // if (promoCode === )
-        
-    })
-       
+    // Esecuzione ciclo for per controllare la validità del codice promo
+    for (let i = 0; i < arrayCodici.length; i++) {
+        const curCode = arrayCodici[i];
+        const sconto = prezzoParziale * 25 / 100;
+
+        if (promoCodeUser.value == curCode) {
+            prezzoFinale = prezzoParziale - sconto;
+        } else if (promoCodeUser.value == !curCode) {
+            prezzoFinale = prezzoParziale;
+        }
+
+    // Iterazione per far apparire messaggio di errore in caso di codice errato      
+        if (promoCodeUser.value == "" || promoCodeUser.value == curCode){
+            promoCodeError.classList.add("d-none");
+        } else  {
+            promoCodeError.classList.remove("d-none")
+        }
+
+      
+    }
+    
+    // Stampa risultato in pagina
+    finalPriceElem.innerText = (`€ ${prezzoFinale.toFixed(2)}`)
 
 });
